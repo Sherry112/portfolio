bf8213f0816043e09d33586ef0861116116d8d3e
Shaheryar.Khan <shaheryar.khan@simplifa.de>
2025-08-18 14:08:24 +0200

    deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@shahkhan',
   },
-  ugins: [
+  plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
