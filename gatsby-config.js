e4d442ccf9758700e4e7fda7bee4a6ec2c5484fe
Shaheryar.Khan <shaheryar.khan@simplifa.de>
2025-08-18 13:44:27 +0200

    Add GitHub Pages deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: '/portfolio', // Add this line for GitHub Pages subdirectory
   siteMetadata: {
     title: 'Brittany Chiang',
     description:
       'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://sherry112.github.io/portfolio', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
